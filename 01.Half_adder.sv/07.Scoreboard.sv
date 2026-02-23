7706b76e77e2d8613bacb8212cf2e7b033f4168a
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-23 17:02:03 +0530

    Create 07.Scoreboard.sv

---
+++ 01.Half_adder.sv/07.Scoreboard.sv
@@ -0,0 +1,17 @@
+class scoreboard;
+  transaction tr;
+  mailbox moni_to_sco;
+  
+  function new(mailbox moni_to_sco);
+    this.moni_to_sco=moni_to_sco;
+  endfunction
+  
+  task sco();
+    tr=new();
+    repeat(5) begin
+      moni_to_sco.get(tr);
+      tr.display("Scoreboard");
+      #2;
+    end
+  endtask
+endclass
