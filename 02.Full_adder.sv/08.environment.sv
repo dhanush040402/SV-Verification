b62b7f3301a1cac511604c227c8584e1d9a056bc
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-24 22:36:58 +0530

    Create 08.environment.sv

---
+++ 02.Full_adder.sv/08.environment.sv
@@ -0,0 +1,38 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  transaction tr;
+  
+  virtual intr inf;
+  generator gen;
+  driver driv;
+  monitor moni;
+  scoreboard sco;
+  
+  mailbox gen_to_driv;
+  mailbox moni_to_sco;
+  
+  function new(virtual intr inf);
+    this.inf=inf;
+    tr=new();
+    gen_to_driv=new();
+    moni_to_sco=new();
+    gen=new(gen_to_driv);
+    driv=new(inf,gen_to_driv);
+    moni=new(inf,moni_to_sco);
+    sco=new(moni_to_sco);
+  endfunction
+  
+  task envi();
+    fork
+      gen.gen();
+      driv.driv();
+      moni.moni();
+      sco.sco();
+    join
+  endtask
+endclass
