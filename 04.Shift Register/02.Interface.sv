f308ceba1ff5fe5f42e45803c7977081bacad08f
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-16 14:40:50 +0530

    Create 02.Interface.sv

---
+++ 04.Shift Register/02.Interface.sv
@@ -0,0 +1,11 @@
+interface intr;
+  logic sin;
+  logic clk;
+  logic rst;
+  logic sout;
+  
+  modport dut(input sin,clk,rst, output sout);
+  modport tb(output sin,clk,rst, input sout);
+  
+endinterface
+
