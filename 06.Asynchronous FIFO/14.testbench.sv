345f6009e999fbdc208281a7ce5d9ad12a795059
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-20 21:36:20 +0530

    Create 14.testbench.sv

---
+++ 06.Asynchronous FIFO/14.testbench.sv
@@ -0,0 +1,41 @@
+`include "test.sv"
+
+module tb;
+  
+  parameter depth = 8 ;
+  parameter data_width = 8 ;
+  
+  variable #(depth,data_width) intr();
+  test #(depth,data_width) t(intr);
+  async_fifo_top #(depth,data_width) f(intr);
+  
+  initial begin
+    
+    intr.wclk = 1 ;
+    forever #7 intr.wclk = ~ intr.wclk;
+    
+  end
+  
+  initial begin
+    
+    intr.rclk = 1 ;
+    forever #5 intr.rclk = ~ intr.rclk;
+    
+  end
+  
+  initial begin
+    $dumpfile("async_fifo.vcd");
+    $dumpvars();
+    
+    #400;
+    $finish;
+      
+  end
+  
+endmodule
+  
+  
+  
+  
+  
+  
