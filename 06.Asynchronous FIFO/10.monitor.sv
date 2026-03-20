13accbd1c901a2c599c8f08bb8ed4c3825b970af
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-20 21:31:06 +0530

    Create 10.monitor.sv

---
+++ 06.Asynchronous FIFO/10.monitor.sv
@@ -0,0 +1,55 @@
+class monitor #(parameter depth , data_width);
+  transaction #( depth , data_width) w_tr , r_tr;
+  mailbox w_montosco;
+  mailbox r_montosco;
+  virtual variable.wm wm1;
+  virtual variable.rm rm1;
+  
+  function new (mailbox w_montosco , mailbox r_montosco , virtual variable.wm wm1 , virtual variable.rm rm1);
+    this.w_montosco = w_montosco;
+    this.r_montosco = r_montosco;
+    this.wm1 = wm1;
+    this.rm1 = rm1;
+  endfunction
+  
+  task main();
+    
+    fork 
+      
+      wmain();
+      rmain();
+       
+    join
+  
+  endtask
+  
+  
+  task wmain();
+    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+      @(posedge wm1.wclk)
+      w_tr = new();
+      w_tr.wrst <= wm1.w_monit.wrst;
+      w_tr.w_en <= wm1.w_monit.w_en;
+      w_tr.data_in <= wm1.w_monit.data_in;
+      w_tr.full <= wm1.w_monit.full;
+      
+      w_montosco.put(w_tr);
+    end
+  endtask
+  
+  
+  task rmain();
+    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+      @(posedge rm1.rclk)
+      r_tr = new();
+      r_tr.rrst <= rm1.r_monit.rrst;
+      r_tr.r_en <= rm1.r_monit.r_en;
+      r_tr.data_out <= rm1.r_monit.data_out;
+      r_tr.empty <= rm1.r_monit.empty;
+      
+      r_montosco.put(r_tr);
+        
+    end
+  endtask
+  
+endclass
