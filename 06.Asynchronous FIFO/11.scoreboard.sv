e38389223d9bbef34f76dbb58c6f5575273c1e73
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-23 20:36:38 +0530

    Update 11.scoreboard.sv

--- 06.Asynchronous FIFO/11.scoreboard.sv
+++ 06.Asynchronous FIFO/11.scoreboard.sv
@@ -1,12 +1,22 @@
 class scoreboard #(parameter depth , data_width);
   transaction #( depth , data_width) w_tr , r_tr;
+  
   mailbox w_montosco;
   mailbox r_montosco;
+  
+  event w_sb_done;
+  event r_sb_done;
+  event w_mo_done;
+  event r_mo_done;
 
   
-  function new (mailbox w_montosco , mailbox r_montosco);
+  function new (mailbox w_montosco , mailbox r_montosco,event w_sb_done,event r_sb_done,event w_mo_done,event r_mo_done);
     this.w_montosco = w_montosco;
     this.r_montosco = r_montosco;
+    this.w_sb_done = w_sb_done;
+    this.r_sb_done = r_sb_done;
+    this.w_mo_done = w_mo_done;
+    this.r_mo_done = r_mo_done;
   endfunction
   
    task main();
@@ -22,16 +32,21 @@ class scoreboard #(parameter depth , data_width);
   
   
   task wmain();
-    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+    for (int i = 0 ; i < ( depth ) ; i++ ) begin
+      @(w_mo_done);
       w_montosco.get(w_tr);
-      w_tr.wdisplay();
+      w_tr.wdisplay("Scoreboard Write");
+      
+      ->w_sb_done;
     end
   endtask
   
   task rmain();
-    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+    for (int i = 0 ; i < ( depth ) ; i++ ) begin
+      @(w_mo_done);
       r_montosco.get(r_tr);
-      r_tr.rdisplay();
+      r_tr.rdisplay("Scoreboard Read");
+      ->r_sb_done;
     end
   endtask
   
