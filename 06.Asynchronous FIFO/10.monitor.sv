519c8ed27d8a03a9b357dac08bd7514c9865ff0e
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-23 20:35:39 +0530

    Update 10.monitor.sv

--- 06.Asynchronous FIFO/10.monitor.sv
+++ 06.Asynchronous FIFO/10.monitor.sv
@@ -1,15 +1,29 @@
 class monitor #(parameter depth , data_width);
   transaction #( depth , data_width) w_tr , r_tr;
+  
   mailbox w_montosco;
   mailbox r_montosco;
+  
   virtual variable.wm wm1;
   virtual variable.rm rm1;
   
-  function new (mailbox w_montosco , mailbox r_montosco , virtual variable.wm wm1 , virtual variable.rm rm1);
+  event w_dr_done;
+  event r_dr_done;
+  event w_mo_done;
+  event r_mo_done;
+  
+  function new (mailbox w_montosco , mailbox r_montosco , virtual variable.wm wm1 , virtual variable.rm rm1,event w_dr_done,event r_dr_done,event w_mo_done,event r_mo_done);
+    
     this.w_montosco = w_montosco;
     this.r_montosco = r_montosco;
     this.wm1 = wm1;
     this.rm1 = rm1;
+    
+    this.w_dr_done = w_dr_done;
+    this.r_dr_done = r_dr_done;
+    this.w_mo_done = w_mo_done;
+    this.r_mo_done = r_mo_done;
+    
   endfunction
   
   task main();
@@ -25,7 +39,8 @@ class monitor #(parameter depth , data_width);
   
   
   task wmain();
-    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+    for (int i = 0 ; i < ( depth ) ; i++ ) begin
+      @(w_dr_done);
       @(posedge wm1.wclk)
       w_tr = new();
       w_tr.wrst <= wm1.w_monit.wrst;
@@ -34,12 +49,16 @@ class monitor #(parameter depth , data_width);
       w_tr.full <= wm1.w_monit.full;
       
       w_montosco.put(w_tr);
+      w_tr.wdisplay("Monitor Write");
+      ->w_mo_done;
+      
     end
   endtask
   
   
   task rmain();
-    for (int i = 0 ; i < ( depth * 2 ) ; i++ ) begin
+    for (int i = 0 ; i < ( depth ) ; i++ ) begin
+      @(r_dr_done);
       @(posedge rm1.rclk)
       r_tr = new();
       r_tr.rrst <= rm1.r_monit.rrst;
@@ -48,6 +67,8 @@ class monitor #(parameter depth , data_width);
       r_tr.empty <= rm1.r_monit.empty;
       
       r_montosco.put(r_tr);
+      r_tr.rdisplay("Monitor Read");
+      ->r_mo_done;
         
     end
   endtask
